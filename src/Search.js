d94a8a724a7ffe71db030e3f3aa5f768174b799d
Kovvuri Jagadeswara Butchi Reddy <114736078+kovvurijag@users.noreply.github.com>
2023-06-27 01:57:51 +0200

    GenerateVIN

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,28 @@
 import React, { useState } from 'react';
-import './Search.css'
-import './Reset.css'
+import './Search.css';
+import './Reset.css';
 import logo from './thumbnail_pek_logo_sm.png';
+import axios from 'axios';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/database';
+
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBYlgpHYRK4v2tSI-zns1nQqe-OlbYTKdA",
+  authDomain: "jagadesh-df81f.firebaseapp.com",
+  databaseURL: "https://jagadesh-df81f.firebaseio.com",
+  projectId: "jagadesh-df81f",
+  storageBucket: "jagadesh-df81f.appspot.com",
+  messagingSenderId: "139022798122",
+  appId: "1:139022798122:web:5a0a4c974c6b813abda253"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
 
 const Search = () => {
   const [version, setVersion] = useState('');
@@ -14,43 +35,32 @@ const Search = () => {
   const [isAddButtonDisabled, setIsAddButtonDisabled] = useState(true);
 
   const handleGenerateVIN = () => {
-    // Perform validation on form fields
     if (!version || !equipmentCode || !yearOfIssue || !placeOfProduction || !serialNumber) {
       alert('Please fill in all fields');
       return;
     }
 
-    // Generate the VIN based on form data
     const vinNumber = generateVIN();
 
-    // Update the generated VIN state
     setGeneratedVIN(vinNumber);
 
-    // Enable the "Add" button
     setIsAddButtonDisabled(false);
   };
 
   const generateVIN = () => {
-    // Pad the version, year of issue, and serial number with leading zeros if necessary
     const paddedVersion = version.toString().padStart(3, '0');
     const paddedYearOfIssue = yearOfIssue.toString().padStart(2, '0');
     const paddedSerialNumber = serialNumber.toString().padStart(6, '0');
-  
-    // Concatenate the VIN components
     const vinNumber = `VVV${paddedVersion}${paddedYearOfIssue}1${paddedSerialNumber}${placeOfProduction}`;
-  
     return vinNumber;
   };
-  
 
   const handleSearch = () => {
-    // Perform validation on form fields
     if (!version || !equipmentCode || !yearOfIssue || !placeOfProduction) {
       alert('Please fill in all fields');
       return;
     }
 
-    // Send a POST request to the server (webtest.pekauto.com) to search for the next available serial number
     const payload = {
       version,
       equipmentCode,
@@ -58,20 +68,12 @@ const Search = () => {
       placeOfProduction
     };
 
-    fetch('http://webtest.pekauto.com/search', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    })
-      .then(response => response.json())
-      .then(data => {
-        // Update the available serial number state
-        setAvailableSerialNumber(data.serialNumber);
+    axios.post('https://jagadesh-df81f.firebaseio.com/search.json',payload)
 
-        // Automatically fill the serial number field in the form
-        setSerialNumber(data.serialNumber);
+      .then(response => {
+        console.log(response.data);
+        setAvailableSerialNumber(response.data.serialNumber);
+        setSerialNumber(response.data.serialNumber);
       })
       .catch(error => {
         console.error('Error:', error);
@@ -79,30 +81,55 @@ const Search = () => {
   };
 
   const handleAdd = () => {
-    // Send a POST request to the server (webtest.pekauto.com) to add the new VIN number
+    if (!generatedVIN) {
+      alert('Please generate a VIN number');
+      return;
+    }
+
     const payload = {
       vinNumber: generatedVIN
     };
 
-    fetch('http://webtest.pekauto.com/add', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(payload)
-    })
+    axios.post('https://jagadesh-df81f.firebaseio.com/add.json',payload)
       .then(response => {
-        // Reset the form fields
-        setVersion('');
-        setEquipmentCode('');
-        setYearOfIssue('');
-        setPlaceOfProduction('');
-        setSerialNumber('');
-        setGeneratedVIN('');
-        setAvailableSerialNumber('');
-        setIsAddButtonDisabled(true);
+        console.log(response.data);
+        if (response.data.success) {
+          setVersion('');
+          setEquipmentCode('');
+          setYearOfIssue('');
+          setPlaceOfProduction('');
+          setSerialNumber('');
+          setGeneratedVIN('');
+          setAvailableSerialNumber('');
+          setIsAddButtonDisabled(true);
 
-        alert('VIN number added successfully!');
+          alert('Failed to add VIN number!');
+        } else {
+          alert('VIN number added successfully.');
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  };
+
+  const saveSearchDataToFirebase = () => {
+    if (!version || !equipmentCode || !yearOfIssue || !placeOfProduction) {
+      alert('Please fill in all fields');
+      return;
+    }
+
+    const payload = {
+      version,
+      equipmentCode,
+      yearOfIssue,
+      placeOfProduction
+    };
+
+    // Save search data to Firebase Realtime Database
+    database.ref('searches').push(payload)
+      .then(() => {
+        console.log('Search data saved to Firebase');
       })
       .catch(error => {
         console.error('Error:', error);
@@ -111,7 +138,7 @@ const Search = () => {
 
   return (
     <div className='box'>
-      <img src={logo} className='logo'/>
+      <img src={logo} className='logo' alt='PEK Logo' />
       <h2>Generate VIN</h2>
       <label>Version:</label>
       <input type="number" value={version} onChange={(e) => setVersion(e.target.value)} />
@@ -146,8 +173,9 @@ const Search = () => {
       <br />
       <button className='search' onClick={handleSearch}>Search</button>
       {availableSerialNumber && <div>Available Serial Number: {availableSerialNumber}</div>}
-     
+
       <button className='add' onClick={handleAdd} disabled={isAddButtonDisabled}>Add</button>
+     
     </div>
   );
 };
